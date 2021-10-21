da0c41f8b7c9048742945e4ae165fea53e765bf1
Maksim Levin <levmaxmih88@mail.ru>
2021-10-21 09:29:16 +0700

    Added data binding

--- app/src/main/java/ru/skillbranch/gameofthrones/ui/SplashFragment.kt
+++ app/src/main/java/ru/skillbranch/gameofthrones/ui/SplashFragment.kt
@@ -1,33 +1,39 @@
 package ru.skillbranch.gameofthrones.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import ru.skillbranch.gameofthrones.R
+import ru.skillbranch.gameofthrones.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
 
+    private var _binding: FragmentSplashBinding? = null
+    private val binding: FragmentSplashBinding
+        get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding == null")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val container = view.findViewById<ImageView>(R.id.splash_image)
-        container.setOnClickListener {
+        binding.splashImage.setOnClickListener {
             findNavController().navigate(
                 SplashFragmentDirections.actionSplashFragmentToHouseFragment()
             )
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
